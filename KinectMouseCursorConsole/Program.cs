32bf3923a8d39d21a0f051f3367f680fb8246b79
tichinose1-surface <toyoh.ichinose@unisys.co.jp>
2016-08-15 19:58:20 +0900

    ざっくり実装

--- KinectMouseCursorConsole/Program.cs
+++ KinectMouseCursorConsole/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using KinectMouseCursorConsole.Properties;
+using Microsoft.Kinect;
+using System;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
 
 namespace KinectMouseCursorConsole
 {
@@ -10,6 +13,46 @@ namespace KinectMouseCursorConsole
     {
         static void Main(string[] args)
         {
+            var sensor = KinectSensor.GetDefault();
+            var reader = sensor.BodyFrameSource.OpenReader();
+            sensor.Open();
+
+            var bodies = Observable.Interval(TimeSpan.FromSeconds(1.0 / Settings.Default.FPS))
+                .Select(_ =>
+                {
+                    using (var frame = reader.AcquireLatestFrame())
+                    {
+                        if (frame == null) return null;
+
+                        var data = new Body[frame.BodyCount];
+                        frame.GetAndRefreshBodyData(data);
+                        return data;
+                    }
+                });
+            var body = bodies
+                .Where(bs => bs != null)
+                .Select(bs => bs.Where(b => b.IsTracked).FirstOrDefault());
+            var handRight = body
+                .Where(b => b != null)
+                .Select(b => b.Joints[JointType.HandRight]);
+            var handRightPosition = handRight
+                .Select(h => h.Position)
+                .Do(p => Debug.WriteLine($"X: {p.X}, Y: {p.Y}"));
+            handRightPosition
+                .Subscribe(p =>
+                {
+                    var x = (1920 / 2) + p.X * Settings.Default.Scale;
+                    var y = (1080 / 2) - p.Y * Settings.Default.Scale;
+
+                    SetCursorPos((int)x, (int)y);
+
+                    if (p.Z > 0.8) Debug.WriteLine("hoge");
+                });
+
+            Console.ReadKey();
         }
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
     }
 }
